53705416ff7ab1faaabd67f0fd644a1ceaa8dfe8
SionChen <759856708@qq.com>
2018-05-04 10:33:28 +0800

    Initial Commit

---
+++ CYXLineChatDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYXLineChatDemo
+//
+//  Created by 超级腕电商 on 2018/5/4.
+//  Copyright © 2018年 超级腕电商. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
